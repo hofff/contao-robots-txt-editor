99efc1e46697392b906e3ee47d940da901eda439
Cliff Parnitzky <github@cliff-parnitzky.de>
2016-06-02 00:06:54 +0200

    Define correct translation

--- CT_ROOT/system/modules/hofff_robots-txt-editor/languages/de/tl_page.php
+++ CT_ROOT/system/modules/hofff_robots-txt-editor/languages/de/tl_page.php
@@ -4,6 +4,6 @@ $GLOBALS['TL_LANG']['tl_page']['robotstxt_legend'] = "robots.txt Editor";
 $GLOBALS['TL_LANG']['tl_page']['createRobotsTxt']  = array("Eine robots.txt erstellen", "Eine individuelle robots.txt erstellen.");
 $GLOBALS['TL_LANG']['tl_page']['robotsTxtContent'] = array("Inhalt der robots.txt", "Geben Sie den Inhalt der robots.txt an.");
 
-$GLOBALS['TL_LANG']['tl_page']['robotsTxtAddAbsoluteSitemapPath'] = array("Add absolute sitemap path to robots.txt", "Add the absolute path of the sitemap to the robots.txt");
+$GLOBALS['TL_LANG']['tl_page']['robotsTxtAddAbsoluteSitemapPath'] = array("Absoluten Sitemap Pfad zu robots.txt hinzufügen", "Den absoluten Pfad der Sitemap zur robots.txt hinzufügen.");
 
 $GLOBALS['TL_LANG']['tl_page']['robotsTxtContentImport'] = array("Default robots.txt importieren", "Die default robots.txt importieren (überschreibt vorhandene Daten).");
